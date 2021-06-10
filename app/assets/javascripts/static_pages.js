f73e5c861c85941415220d30cc9510e79933f5b9
felipeberger <feconchaber@gmail.com>
2021-06-10 16:44:29 +1100

    update to static_pages to add checkboxes and remove buttons

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -2,9 +2,9 @@ $(document).on("turbolinks:load", function () {
     if ($('.static_pages.index').length > 0) {
       indexTasks(function (response) {
         var htmlString = response.tasks.map(function(task) {
-          return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'> \
-            " + task.content + "\
-            </div>";
+          return "<div class='col-12 mb-3 p-2 border rounded task clearfix' data-id='" + task.id + "'> \
+          <input type='checkbox' class='mark-complete mr-2'>" + task.content + "\
+            <button class='delete float-right " + task.completed + "' data-id='" + task.id + "'>Remove</button></div>";
         });
   
         $("#tasks").html(htmlString);
