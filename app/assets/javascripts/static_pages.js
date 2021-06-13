996730bc68b016b4e9f6f11b02c09eacbfa2b513
felipeberger <feconchaber@gmail.com>
2021-06-13 21:21:52 +1100

    WiP - the "create" button now sends the post request to the api. Next step is to get the page to reload on sucess

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -4,10 +4,28 @@ $(document).on("turbolinks:load", function () {
         var htmlString = response.tasks.map(function(task) {
           return "<div class='col-12 mb-3 p-2 border rounded task clearfix' data-id='" + task.id + "'> \
           <input type='checkbox' class='mark-complete mr-2'>" + task.content + "\
-            <button class='delete float-right " + task.completed + "' data-id='" + task.id + "'>Remove</button></div>";
+          <button class='delete rounded float-right " + task.completed + "' data-id='" + task.id + "'>Remove</button></div>";
         });
   
         $("#tasks").html(htmlString);
       });
-    }
+    };
+
+    $('#create-task').on('submit', function (e) {
+      e.preventDefault();
+      var content = $('#new-task-content').val();
+      postTask(content);
+      $('#new-task-content').val('');
+    });
+
+    // var injectTask = indexTasks(function (response) {
+    //   var htmlString = response.tasks.map(function(task) {
+    //     return "<div class='col-12 mb-3 p-2 border rounded task clearfix' data-id='" + task.id + "'> \
+    //     <input type='checkbox' class='mark-complete mr-2'>" + task.content + "\
+    //     <button class='delete rounded float-right " + task.completed + "' data-id='" + task.id + "'>Remove</button></div>";
+    //   });
+    
+    //   $("#tasks").html(htmlString);
+    // });
+
   });
