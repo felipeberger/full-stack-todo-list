bb964c1f536969cbe9bb4e91b951ba51a17fb1d4
felipeberger <feconchaber@gmail.com>
2021-06-17 10:54:16 +1100

    Updated static_pages to mark tasks complete (checkbox checked and text strikethrough). Also modified the HTML manupulation method so it renders tasks checked or not depending on the data returning from the API call (previous version would have the tasks re-render on refresh  with checkbox unchecked and text not struck through). Minor edits to index.html.erb page

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,86 +1,94 @@
 $(document).on("turbolinks:load", function () {
-    if ($('.static_pages.index').length > 0) {
-      indexTasks(function (response) {
-        var htmlString = response.tasks.map(function(task) {
-          return "<div class='col-12 mb-3 p-2 border rounded task clearfix' data-id='" + task.id + "'> \
-          <input type='checkbox' class='mark-complete mr-2'><span style=''>" + task.content + "\
-          </span><button class='delete rounded float-right " + task.completed + "' data-id='" + task.id + "'>Remove</button></div>";
-        });
-  
-        $("#tasks").html(htmlString);
-      });
-    };
+  if ($('.static_pages.index').length > 0) {
+    indexTasks(function (response) {
+      injectTask(response)
+    });
+  };
 
     $('#create-task').on('submit', function (e) {
       e.preventDefault();
       var content = $('#new-task-content').val();
       postTask(content, () => {
         indexTasks(function (response) {
-          var htmlString = response.tasks.map(function(task) {
-            return "<div class='col-12 mb-3 p-2 border rounded task clearfix' data-id='" + task.id + "'> \
-            <input type='checkbox' class='mark-complete mr-2'><span style=''>" + task.content + "\
-            </span><button class='delete rounded float-right " + task.completed + "' data-id='" + task.id + "'>Remove</button></div>";
-          });
-    
-          $("#tasks").html(htmlString);
+          injectTask(response)
         });
       });
+
       $('#new-task-content').val('');
     });
 
-    // injectTask = () => {
-    //   if ($('.static_pages.index').length > 0) {
-    //     indexTasks(function (response) {
-    //       var htmlString = response.tasks.map(function(task) {
-    //         return "<div class='col-12 mb-3 p-2 border rounded task clearfix' data-id='" + task.id + "'> \
-    //         <input type='checkbox' class='mark-complete mr-2'>" + task.content + "\
-    //         <button class='delete rounded float-right " + task.completed + "' data-id='" + task.id + "'>Remove</button></div>";
-    //       });
-    
-    //       $("#tasks").html(htmlString);
-    //     });
-    //   };
-    // };
-
 });
 
-// var injectTask = function () {
+var injectTask = (response) => {
+  var htmlString = response.tasks.map(function (task) {
 
-//     indexTasks(function (response) {
-//       var htmlString = response.tasks.map(function(task) {
-//         return "<div class='col-12 mb-3 p-2 border rounded task clearfix' data-id='" + task.id + "'> \
-//         <input type='checkbox' class='mark-complete mr-2'>" + task.content + "\
-//         <button class='delete rounded float-right " + task.completed + "' data-id='" + task.id + "'>Remove</button></div>";
-//       });
-//     });
-//   };
-
+    return "<div class='col-12 mb-3 p-2 border rounded task clearfix "+ task.completed +"' data-id='" + task.id + "'> \
+        <input type='checkbox' class='mark-complete mr-2'" + (task.completed ? 'checked' : '') + "><span style='" + (task.completed ? 'text-decoration: line-through' : '') + "'>" + task.content + "\
+        </span><button class='delete rounded float-right " + task.completed + "' data-id='" + task.id + "'>Remove</button></div>";
+  });
+  $("#tasks").html(htmlString);
+};
 
 $(document).on('click', '.delete', function(e) {
   e.preventDefault();
   var divDataId = $(this).closest('div').attr('data-id');
   deleteTask(divDataId, () => {
     indexTasks(function (response) {
-      var htmlString = response.tasks.map(function(task) {
-        return "<div class='col-12 mb-3 p-2 border rounded task clearfix' data-id='" + task.id + "'> \
-        <input type='checkbox' class='mark-complete mr-2'><span style=''>" + task.content + "\
-        </span><button class='delete rounded float-right " + task.completed + "' data-id='" + task.id + "'>Remove</button></div>";
-      });
-
-      $("#tasks").html(htmlString);
+      injectTask(response)
     });
   });
 });
 
-$(document).on('click', '.mark-complete', function () {
-  var checkComplete = $(this).next().attr('style').length;
-  var selection = $(this).next()
-  console.log(checkComplete)
+$(document).on('click', '.mark-complete', function (e) {
 
-  if (checkComplete == 0) {
-    selection.attr('style', 'text-decoration: line-through')
+  e.preventDefault()
+  var selectSpan = $(this).next()
+  var checkComplete = $(this).closest('div').attr('class')
+  var divSelect = $(this).closest('div')
+  var divDataId = $(this).closest('div').attr('data-id')
+  var queryConstructor = "[data-id='" + String(divDataId) + "']";
+
+  if (checkComplete.includes('false')) {
+    selectSpan.attr('style', 'text-decoration: line-through')
+    divSelect.removeClass('false')
+    divSelect.addClass('true')
+    document.querySelector(queryConstructor).querySelector('input').outerHTML = "<input type='checkbox' class='mark-complete mr-2' checked></input>"
+
+    markInactive(divDataId)
+
+
   } else {
     selection.attr('style', '')
+    divSelect.removeClass('true')
+    divSelect.addClass('false')
+    document.querySelector(queryConstructor).querySelector('input').outerHTML = "<input type='checkbox' class='mark-complete mr-2' ></input>"
+
+    markActive(divDataId)
+
   };
 
 });
+
+$(document).on('click','.visibility-button', function (e) {
+  e.preventDefault();
+  var buttonId = $(this).attr('id');
+  var allTasks = $('#tasks').children();
+
+  switch (buttonId) {
+    case 'all-tasks':
+      // something
+
+      break;
+    case 'active-only':
+      // something
+      // allTasks.each(function(){
+      //   $(this).attr('class', '')
+      // })
+
+
+      break;
+    case 'completed-only':
+      // something
+  }
+
+})
